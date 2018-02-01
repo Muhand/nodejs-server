193486a9c5ae7949ea175a72347f7704c771cb1b
Muhand Jumah <muhandjumah@Muhands-MBP.fios-router.home>
2018-02-01 18:19:56 -0500

    Added database initialization to routes

--- src/app/api/v1/routes/index.js
+++ src/app/api/v1/routes/index.js
@@ -2,11 +2,13 @@ import express from 'express';
 import controllersRouter from '../controllers/';
 import jobs from '../jobs/'
 import payloads from '../payloads/'
+import initializeDb from '../../../../db';
 
 let router = express();
 
-//'/v1'
-router.use('/', controllersRouter.Home({jobs, payloads}));
-
+initializeDb(db => {
+  //'/v1'
+  router.use('/', controllersRouter.Home({jobs, payloads}));
+});
 
 export default router;
